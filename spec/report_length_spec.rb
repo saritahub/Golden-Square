2c4ec4bf502df091b36801c053ec1e1e4c5ed87c
Sarita <saritaradia65@gmail.com>
2022-09-13 17:46:59 +0100

    Report Length challenge

---
+++ spec/report_length_spec.rb
@@ -0,0 +1,16 @@
+require '/Users/saritaradia/Desktop/Projects/Golden-Square/lib/01-Testing-Methods/report_length.rb'
+
+RSpec.describe "Check string length" do
+    it "returns 'This string was 5 characters long'" do
+        result = report_length("horse")
+        expect(result).to eq "This string was 5 characters long."
+    end 
+    it "returns 'This string was 5 characters long'" do
+        result = report_length("upgraded iPhone")
+        expect(result).to eq "This string was 15 characters long."
+    end 
+    it "returns 'This string was 5 characters long'" do
+        result = report_length("")
+        expect(result).to eq "This string was 0 characters long."
+    end 
+end 
