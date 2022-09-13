0df916d986210bbe30a873fc46d09a68b72b9589
Sarita <saritaradia65@gmail.com>
2022-09-13 15:33:40 +0100

    Chapter 2 Question 2

---
+++ spec/Chapter2Two_spec.rb
@@ -0,0 +1,45 @@
+require '/Users/saritaradia/Desktop/Projects/Golden-Square/lib/01-Testing-Methods/Chapter2Two.rb'
+
+RSpec.describe StringBuilder do 
+    it "Counts the length of strings" do 
+       stringbuilder = StringBuilder.new
+       stringbuilder.add("tree")
+       result = stringbuilder.size 
+       expect(result).to eq 4
+    end
+    it "Counts the length of strings" do 
+        stringbuilder = StringBuilder.new
+        stringbuilder.add("treehouse")
+        result = stringbuilder.size 
+        expect(result).to eq 9
+     end
+     it "Returns string str" do 
+        stringbuilder = StringBuilder.new
+        stringbuilder.add("tree")
+        result = stringbuilder.output
+        expect(result).to eq "tree"
+     end
+     it "Returns string str" do 
+        stringbuilder = StringBuilder.new
+        stringbuilder.add("treehouse")
+        result = stringbuilder.output
+        expect(result).to eq "treehouse"
+     end
+
+     it "Counts the length of strings" do 
+        stringbuilder = StringBuilder.new
+        stringbuilder.add("tree")
+        stringbuilder.add("house")
+        result = stringbuilder.size 
+        expect(result).to eq 9
+     end
+
+     it "Returns string str" do 
+        stringbuilder = StringBuilder.new
+        stringbuilder.add("tree")
+        stringbuilder.add(" ")
+        stringbuilder.add("house")
+        result = stringbuilder.output
+        expect(result).to eq "tree house"
+     end
+end 
