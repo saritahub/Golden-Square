dd72675179117fe98faad83c4f344113e643e885
Sarita <saritaradia65@gmail.com>
2022-09-13 18:00:59 +0100

    Reminder task

--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -1,10 +1,23 @@
-# require '/Users/saritaradia/Desktop/Projects/Golden-Square/lib/03-Testing-For-Errors/reminder.rb'
+require '/Users/saritaradia/Desktop/Projects/Golden-Square/lib/02-Testing-Classes/reminder.rb'
 
-# RSpec.describe Reminder do
-#   context "when no task is set" do
-#     it "fails" do
-#       reminder = Reminder.new("Kay")
-#       expect { reminder.remind() }.to raise_error "No reminder set!"
-#     end
-#   end
-# end
+RSpec.describe Reminder do
+  it "Returns the task and name" do
+    reminder = Reminder.new("Sam") #Name
+    reminder.remind_me_to("Bake")
+    result = reminder.remind()
+    expect(result).to eq "Bake, Sam!"
+  end 
+  it "Returns the task and name" do
+    reminder = Reminder.new("Mike") #Name
+    reminder.remind_me_to("Run")
+    result = reminder.remind()
+    expect(result).to eq "Run, Mike!"
+  end 
+  it "Returns the task and name" do
+    reminder = Reminder.new("Sarah") #Name
+    reminder.remind_me_to("")
+    result = reminder.remind()
+    expect(result).to eq ", Sarah!"
+  end 
+
+end 
