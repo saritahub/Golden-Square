5452b2291cb193c57c38aaec982e4901bb80afea
Sarita <saritaradia65@gmail.com>
2022-09-14 12:07:28 +0100

    Testing for errors 03

---
+++ spec/reminder_03_spec.rb
@@ -0,0 +1,12 @@
+require '/Users/saritaradia/Desktop/Projects/Golden-Square/lib/03-Testing-For-Errors/reminder_03.rb'
+
+RSpec.describe Reminder do
+    context "when no task is set" do
+      it "fails" do
+        reminder = Reminder.new("Test")
+        expect { reminder.remind() }.to raise_error "No reminder set!"
+      end
+    end
+  end
+
+ 
